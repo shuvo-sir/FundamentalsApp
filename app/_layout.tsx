b4dc18c157412743ef368449eb3092bdb3dfd1d4
Shuvo Halder <shuvohalder499@gmail.com>
2025-07-02 22:21:13 +0600

    create a layout for all same frame

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,36 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>My Expo App</Text>
+      </View>
+      <Slot />
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>@ Shuvo Halder</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "red",
+    padding: 20,
+  },
+  headerText: {
+    color: "white",
+  },
+  footer: {
+    backgroundColor: "blue",
+    padding: 20,
+  },
+  footerText: {
+    color: "white"
+  },
+});
---
+++ app/products/_layout.tsx
@@ -0,0 +1,25 @@
+import { Slot } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function ProductsLayout() {
+    return (
+        <View style={styles.container}>
+            <slot/>
+            <View style={styles.discountedProducts}>
+                <Text>Discounted Products</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    discountedProducts: {
+        backgroundColor: "orange",
+        padding: 20,
+    },
+})
